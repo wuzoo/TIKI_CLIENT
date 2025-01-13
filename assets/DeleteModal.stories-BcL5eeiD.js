c976693355872ed029e259c48d0ef6cae4f1aed1
wuzoo <wuzoo@users.noreply.github.com>
2025-01-13 09:19:23 +0000

    deploy: c02844064159eba0167055dba72d015c7908f756

---
+++ assets/DeleteModal.stories-BcL5eeiD.js
@@ -0,0 +1,9 @@
+import{j as p,a as r,F as i}from"./emotion-react-jsx-runtime.browser.esm-DuDc4FiD.js";import{M as s,u as l}from"./ModalFunnel-C4hYalaQ.js";import"./iframe-TVDyeFV2.js";import"./jsx-runtime-DmVdQlXv.js";import"./index-yYvj8HMH.js";import"./_commonjsHelpers-DDhuufbJ.js";import"./index-DJAF27h-.js";import"./index-Ck13B7A1.js";import"./emotion-react.browser.esm-FCgScvrx.js";import"./index-BLZylyQv.js";import"./QueryClientProvider-DCuuuuK_.js";import"./path-h-hviOD4.js";import"./index-BlQq_uBg.js";import"./ellipsis-DRBQ1vjB.js";import"./file-DTZZ6GXs.js";import"./date-T2SHP4Mx.js";import"./useOutsideClick-DJWaH6sq.js";import"./useOverlay-BQFhv87t.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},o=new e.Error().stack;o&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[o]="756eee19-238f-4a63-a270-06724e27ebe9",e._sentryDebugIdIdentifier="sentry-dbid-756eee19-238f-4a63-a270-06724e27ebe9")}catch{}})();const S={title:"Shared/Modal/Deleted",component:s,parameters:{layout:"centered"}},t={render:()=>{const e=l();return p(i,{children:[r("button",{onClick:()=>e("deleted"),children:"Open Trash Deleted Modal"}),r(s,{})]})}};var n,d,a;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => {
+    const openModal = useOpenModal();
+    return <>
+        <button onClick={() => openModal('deleted')}>Open Trash Deleted Modal</button>
+        <ModalFunnel />
+      </>;
+  }
+}`,...(a=(d=t.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const T=["Deleted"];export{t as Deleted,T as __namedExportsOrder,S as default};
